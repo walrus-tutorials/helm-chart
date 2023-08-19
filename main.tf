fed9613bab75706fbc74d94de4a6edf74f03faa1
gitlawr <lawrleegle@gmail.com>
2023-08-19 15:20:18 +0800

    init commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "service" {
+  namespace = var.walrus_metadata_namespace_name
+  name  = var.walrus_metadata_service_name
+
+  repository = var.chart_repository
+  chart = local.chart
+  version = var.chart_version
+
+  values = [
+    yamlencode(var.values),
+  ]
+}
+
+locals {
+  chart     = coalesce(var.chart_url, var.chart_name)
+}
---
+++ variables.tf
@@ -0,0 +1,53 @@
+# @options ["url", "repository"]
+variable "source_type" {
+  type = string
+  description = "Source type of the chart"
+  default = "url"
+}
+
+# @show_if "source_type=url"
+variable "chart_url" {
+  type = string
+  description = "URL to download the chart"
+  default = ""
+}
+
+# @show_if "source_type=repository"
+variable "chart_repository" {
+  type = string
+  description = "Chart repository"
+  default = ""
+}
+
+# @show_if "source_type=repository"
+variable "chart_name" {
+  type = string
+  description = "Chart name"
+  default = ""
+}
+
+# @show_if "source_type=repository"
+variable "chart_version" {
+  type = string
+  description = "Chart version"
+  default = ""
+}
+
+variable "values" {
+  description = "Chart values in yaml format"
+  default = null
+}
+
+# @hidden
+variable "walrus_metadata_namespace_name" {
+  type        = string
+  description = "Walrus metadata namespace name."
+  default     = ""
+}
+
+# @hidden
+variable "walrus_metadata_service_name" {
+  type        = string
+  description = "Walrus metadata service name."
+  default     = ""
+}
